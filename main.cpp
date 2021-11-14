504509d60c5bef09e4b06ad3bfa90ada83eee3ec
UmeshRanganathan <91838934+UmeshRanganathan@users.noreply.github.com>
2021-11-14 20:50:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-   return 0;
+int main(){
+  int n1=0,n2=1,n3,i,serie;
+  cout <<"numero di serie: ";
+  cin>> serie;
+  
+  
+  if (serie>=2){
+      cout<<n1 <<" "<< n2 << " ";
+  for(i=2;i<serie;++i)
+  {
+  n3=n1+n2;
+  cout<<n3 <<" ";
+  n1=n2;
+  n2=n3;
+  }
+   }
+ else{
+     cout << "errore";
+ }
+ return 0;
 }
