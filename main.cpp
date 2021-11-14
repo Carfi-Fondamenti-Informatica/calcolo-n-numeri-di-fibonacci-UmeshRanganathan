0ac63d70d550948cd3db1d1f26b1134c3cd87b89
UmeshRanganathan <91838934+UmeshRanganathan@users.noreply.github.com>
2021-11-14 23:59:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n=0, i=i,b=1,a=0,c;
+int main(){
+    int n=0,b=1,a=0,c=0;
     cin>>n;
     
     cout<<b<<endl;
-    for (i=0;i<n;i++){
+    for(c=0;c<n;c++)
+    {
         c=a+b;
         cout<<c<<endl;
         a=b;
         b=c;
     }
-    
-    
     return 0;
 }
  
