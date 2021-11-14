c194211b4678677ec31899e3d86f1388520f1cf6
UmeshRanganathan <91838934+UmeshRanganathan@users.noreply.github.com>
2021-11-14 21:04:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
+
 using namespace std;
-int main(){
-  int n,i=1,b=1,a=0,c;
-  cin>>n;
-  
-  if(n>=2)
-  {
-    cout<<b<<endl;
-    for(i=2;i<=n,i++)
+
+int main()
+{
+    int n,i=1,b=1,a=0,c;
+    cin>>n;
+    
+    
+    if(n>=2)
     {
-      c=a*b;
-      cout<<c<<endl;
-      a=b;
-      b=c;
-      
-    }
+        cout<<endl;
+        for(i=2;i<=n;i++)
+        {
+            
+            c=a+b;
+            cout<<c<<endl;
+            a=b;
+            b=c;
+            
+        }
     
+        
     
-  }
-  else
-  {
-    cout<<"errore"<<endl;
-  }
-  return 0;
+        }
+    else
+       {
+           cout<<"errore"<<endl;
+       }
+            
+        return 0;
+    
 }
